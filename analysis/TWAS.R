09d01e6a22e7d2a7a604fb3cd1d66cb6be2752fb
MarcinG <grabe686@gmail.com>
2023-02-13 09:22:24 +0100

    update

--- analysis/TWAS.R
+++ analysis/TWAS.R
@@ -1,11 +1,8 @@
 library(data.table)
 library(tidyverse)
-library(sommer)
-
-K <- as.matrix(bigmemory::attach.big.matrix("../../BigData/Maize/v4/Genotypes/WiDiv_RNAseq/rMVP/WiDiv752.kin.desc"))
-ind <- read.table("../../BigData/Maize/v4/Genotypes/WiDiv_RNAseq/rMVP/WiDiv752.geno.ind")
 
 blup <- fread("../Data/Maize_NPQ_Natural_Variation/data/work/blups/blup2020gwasRNA.csv")
+blup2 <- fread("../Data/Maize_NPQ_Natural_Variation/data/work/blups/blup2021gwasRNA.csv")
 ex <- fread("../Data/Maize_NPQ_Natural_Variation/data/work/expression/WiDiv_Expression_MetaData_csv.gz")
 gene <- fread("../Data/Maize_NPQ_Natural_Variation/data/work/expression/WiDiv_Gene_MetaData_csv.gz")
 
@@ -15,28 +12,20 @@ ex.2 <- ex[ex$Study=="WiDiv-942", -c(2:3)]
 ex.1.pc <- ex.1[,1:6]
 ex.1 <- ex.1[,-c(2:6)]
 
-ex.2.pc <- ex.1[,1:6]
-ex.2 <- ex.1[,-c(1:6)]
+ex.2.pc <- ex.2[,1:6]
+ex.2 <- ex.2[,-c(2:6)]
 
-pheno <- blup[,c(1,11)]
+pheno <- blup[,-c(2,3)]
 
-phenotype <- pheno
-expressionMatrix <- ex.1
-covariate <- ex.1.pc
-
-commonName <- colnames(phenotype)[1]
-
-X <- merge(phenotype, expressionMatrix, by=commonName)
+X <- merge(pheno, ex.1, by="Taxa")
 X <- as.data.frame(X)
 
 toRemove <- which(as.vector(is.na(X[,2])))
 
 X <- X[-c(toRemove),]  
-covariate <- covariate[-c(toRemove),]
+ex.1.pc <- ex.1.pc[-c(toRemove),]
 
-ph <- as.numeric(X[,2])
-
-genExp <- X[,-c(1,2)]
+genExp <- X[,-c(1:33)]
 
 genToKeep <- apply(genExp, 2, FUN = function(x) {
   b <- table(x > 0)
@@ -48,45 +37,158 @@ genToKeep <- which(genToKeep==TRUE)
 genExp <- genExp[,genToKeep]
 genExp <- as.data.frame(genExp)
 
-covariate <- as.matrix(covariate[,-c(1)])
+ex.1.pc <- as.matrix(ex.1.pc[,-c(1)])
 
-res <- apply(genExp, 2, function(x) 
-  summary(lm(ph ~ covariate + x))$coefficients[7,4])
+res <- gene
+res <- res[res$gene %in% colnames(genExp)]
 
-res <- data.frame(gene=names(res), p_val=res)
+for (i in 2:32){
+  ph <- X[,i]
 
-res <- merge(res, gene)
+  a <- apply(genExp, 2, function(x) 
+    summary(lm(ph ~ ex.1.pc + x))$coefficients[7,4])
 
-rownames(K) <- ind$V1
-colnames(K) <- ind$V1
+  a <- data.frame(gene=names(a), p_val=a)
+  colnames(a)[2] <- colnames(X)[i]
 
-z <- data.frame(ph=ph, covariate, genExp)
-z <- data.frame(Taxa=X$Taxa, z)
-
-m <- mmer(ph ~  PC1 + PC2 + PC3,
-          random=~vsr(Taxa,Gu=K),
-          rcov=~units, nIters=3,
-          data=z, verbose = FALSE)
-
-resMix <- data.frame(gen=c(), pmlm=c())
-
-for (i in 25001:ncol(z)){
-  
-  q <- z[,c(1:5,i)]
-  
-  g <- colnames(q)[6]
-  
-  colnames(q)[6] <- 'gene'
-  
-  m2 <- mmer(ph ~  PC1 + PC2 + PC3 + gene,
-             random=~vsr(Taxa,Gu=K),
-             rcov=~units, nIters=3,
-             data=q, verbose = FALSE)
-  a <- anova(m, m2)
-  aa <- data.frame(gene=g, pmlm=a$PrChisq[2])
-  resMix <- rbind.data.frame(resMix, aa)
+  res <- merge(res, a, by="gene")
+  rm(a)
+  print(paste(colnames(X)[i]))
 }
 
-write.csv(res, "GLM_TWAS_1.csv", row.names = F)
-write.csv(resMix, "MLM_TWAS_6.csv", row.names = F)
+######
+###
+######
+
+X <- merge(pheno, ex.2, by="Taxa")
+X <- as.data.frame(X)
+
+toRemove <- which(as.vector(is.na(X[,2])))
+
+X <- X[-c(toRemove),]  
+ex.2.pc <- ex.2.pc[-c(toRemove),]
+
+genExp <- X[,-c(1:33)]
+
+genToKeep <- apply(genExp, 2, FUN = function(x) {
+  b <- table(x > 0)
+  z <- as.numeric(b["TRUE"]) > (nrow(genExp)/2)
+  return(z)}
+)
+
+genToKeep <- which(genToKeep==TRUE)
+genExp <- genExp[,genToKeep]
+genExp <- as.data.frame(genExp)
+
+ex.2.pc <- as.matrix(ex.2.pc[,-c(1)])
+
+res2 <- gene
+res2 <- res2[res2$gene %in% colnames(genExp)]
+
+for (i in 2:32){
+  ph <- X[,i]
+  
+  a <- apply(genExp, 2, function(x) 
+    summary(lm(ph ~ ex.2.pc + x))$coefficients[7,4])
+  
+  a <- data.frame(gene=names(a), p_val=a)
+  colnames(a)[2] <- colnames(X)[i]
+  
+  res2 <- merge(res2, a, by="gene")
+  rm(a)
+  print(paste(colnames(X)[i]))
+}
+
+###
+
+pheno <- blup2[,-c(2,3)]
+
+X <- merge(pheno, ex.1, by="Taxa")
+X <- as.data.frame(X)
+
+toRemove <- which(as.vector(is.na(X[,2])))
+
+X <- X[-c(toRemove),]  
+ex.1.pc <- ex.1.pc[-c(toRemove),]
+
+genExp <- X[,-c(1:33)]
+
+genToKeep <- apply(genExp, 2, FUN = function(x) {
+  b <- table(x > 0)
+  z <- as.numeric(b["TRUE"]) > (nrow(genExp)/2)
+  return(z)}
+)
+
+genToKeep <- which(genToKeep==TRUE)
+genExp <- genExp[,genToKeep]
+genExp <- as.data.frame(genExp)
+
+ex.1.pc <- as.matrix(ex.1.pc[,-c(1)])
+
+res3 <- gene
+res3 <- res3[res3$gene %in% colnames(genExp)]
+
+for (i in 2:32){
+  ph <- X[,i]
+  
+  a <- apply(genExp, 2, function(x) 
+    summary(lm(ph ~ ex.1.pc + x))$coefficients[7,4])
+  
+  a <- data.frame(gene=names(a), p_val=a)
+  colnames(a)[2] <- colnames(X)[i]
+  
+  res3 <- merge(res3, a, by="gene")
+  rm(a)
+  print(paste(colnames(X)[i]))
+}
+
+###
+
+pheno <- blup2[,-c(2,3)]
+
+X <- merge(pheno, ex.2, by="Taxa")
+X <- as.data.frame(X)
+
+toRemove <- which(as.vector(is.na(X[,2])))
+
+X <- X[-c(toRemove),]  
+ex.2.pc <- ex.2.pc[-c(toRemove),]
+
+genExp <- X[,-c(1:33)]
+
+genToKeep <- apply(genExp, 2, FUN = function(x) {
+  b <- table(x > 0)
+  z <- as.numeric(b["TRUE"]) > (nrow(genExp)/2)
+  return(z)}
+)
+
+genToKeep <- which(genToKeep==TRUE)
+genExp <- genExp[,genToKeep]
+genExp <- as.data.frame(genExp)
+
+ex.2.pc <- as.matrix(ex.2.pc[,-c(1)])
+
+res4 <- gene
+res4 <- res4[res4$gene %in% colnames(genExp)]
+
+for (i in 2:32){
+  ph <- X[,i]
+  
+  a <- apply(genExp, 2, function(x) 
+    summary(lm(ph ~ ex.2.pc + x))$coefficients[7,4])
+  
+  a <- data.frame(gene=names(a), p_val=a)
+  colnames(a)[2] <- colnames(X)[i]
+  
+  res4 <- merge(res4, a, by="gene")
+  rm(a)
+  print(paste(colnames(X)[i]))
+}
+
+###
+
+fwrite(res, "../Data/Maize_NPQ_Natural_Variation/bigResults/TWAS/2020/TWAS_WiDiv503.csv.gz")
+fwrite(res2, "../Data/Maize_NPQ_Natural_Variation/bigResults/TWAS/2020/TWAS_WiDiv942.csv.gz")
+fwrite(res3, "../Data/Maize_NPQ_Natural_Variation/bigResults/TWAS/2021/TWAS_WiDiv503_2021.csv.gz")
+fwrite(res4, "../Data/Maize_NPQ_Natural_Variation/bigResults/TWAS/2021/TWAS_WiDiv942_2021.csv.gz")
 
---
+++ nic2.R
@@ -0,0 +1,139 @@
+library(tidyverse)
+library(data.table)
+library(ggsci)
+library(patchwork)
+
+# Set them to classic and axis text to black rather then grey
+theme_set(theme_classic(base_size = 19))
+theme_update(axis.text.x = element_text(colour = "black"), axis.text.y = element_text(colour = "black"))
+
+res <- fread("../Data/Maize_NPQ_Natural_Variation/bigResults/TWAS/2020/TWAS_WiDiv503.csv.gz", data.table = F)
+res2 <- fread("../Data/Maize_NPQ_Natural_Variation/bigResults/TWAS/2020/TWAS_WiDiv942.csv.gz", data.table = F)
+res3 <- fread("../Data/Maize_NPQ_Natural_Variation/bigResults/TWAS/2021/TWAS_WiDiv503_2021.csv.gz", data.table = F)
+res4 <- fread("../Data/Maize_NPQ_Natural_Variation/bigResults/TWAS/2021/TWAS_WiDiv942_2021.csv.gz", data.table = F)
+
+res$chromosome <- as.numeric(res$chromosome)
+res <- res[res$chromosome %in% c(1:10),]
+
+res2$chromosome <- as.numeric(res2$chromosome)
+res2 <- res2[res2$chromosome %in% c(1:10),]
+
+res3$chromosome <- as.numeric(res3$chromosome)
+res3 <- res3[res3$chromosome %in% c(1:10),]
+
+res4$chromosome <- as.numeric(res4$chromosome)
+res4 <- res4[res4$chromosome %in% c(1:10),]
+
+nCHR <- length(unique(res$chromosome))
+res$BPcum <- NA
+s <- 0
+nbp <- c()
+for (i in sort(unique(res$chromosome))){
+  nbp[i] <- max(res[res$chromosome == i,]$position_left)
+  res[res$chromosome == i,"BPcum"] <- res[res$chromosome == i,"position_left"] + s
+  s <- s + nbp[i]
+}
+
+nCHR <- length(unique(res2$chromosome))
+res2$BPcum <- NA
+s <- 0
+nbp <- c()
+for (i in sort(unique(res2$chromosome))){
+  nbp[i] <- max(res2[res2$chromosome == i,]$position_left)
+  res2[res2$chromosome == i,"BPcum"] <- res2[res2$chromosome == i,"position_left"] + s
+  s <- s + nbp[i]
+}
+
+nCHR <- length(unique(res$chromosome))
+res3$BPcum <- NA
+s <- 0
+nbp <- c()
+for (i in sort(unique(res3$chromosome))){
+  nbp[i] <- max(res3[res3$chromosome == i,]$position_left)
+  res3[res3$chromosome == i,"BPcum"] <- res3[res3$chromosome == i,"position_left"] + s
+  s <- s + nbp[i]
+}
+
+nCHR <- length(unique(res$chromosome))
+res4$BPcum <- NA
+s <- 0
+nbp <- c()
+for (i in sort(unique(res4$chromosome))){
+  nbp[i] <- max(res4[res4$chromosome == i,]$position_left)
+  res4[res4$chromosome == i,"BPcum"] <- res4[res4$chromosome == i,"position_left"] + s
+  s <- s + nbp[i]
+}
+
+axis.set <- res %>% 
+  group_by(chromosome) %>% 
+  summarize(center = (max(BPcum) + min(BPcum)) / 2, maxBP=max(BPcum))
+
+g1 <- ggplot() + 
+  geom_hline(yintercept = -log10(0.05/nrow(res)) , linetype=2) +
+  geom_point(data=res, aes(BPcum, -log10(NPQmaxme), colour=as.character(chromosome))) + 
+  scale_color_d3() + 
+  scale_x_continuous(label = axis.set$chromosome, breaks = axis.set$center) + 
+  theme(legend.position = "none") + 
+  ylab("-log10(p-value)") +  
+  xlab("Chromosome")
+
+g2 <- ggplot() + 
+  geom_hline(yintercept = -log10(0.05/nrow(res2)) , linetype=2) +
+  geom_point(data=res2, aes(BPcum, -log10(NPQmaxme), colour=as.character(chromosome))) + 
+  scale_color_d3() + 
+  scale_x_continuous(label = axis.set$chromosome, breaks = axis.set$center) + 
+  theme(legend.position = "none") + 
+  ylab("-log10(p-value)") +  
+  xlab("Chromosome")
+
+g3 <- ggplot() + 
+  geom_hline(yintercept = -log10(0.05/nrow(res3)) , linetype=2) +
+  geom_point(data=res3, aes(BPcum, -log10(NPQmaxme), colour=as.character(chromosome))) + 
+  scale_color_d3() + 
+  scale_x_continuous(label = axis.set$chromosome, breaks = axis.set$center) + 
+  theme(legend.position = "none") + 
+  ylab("-log10(p-value)") +  
+  xlab("Chromosome")
+
+g4 <- ggplot() + 
+  geom_hline(yintercept = -log10(0.05/nrow(res4)) , linetype=2) +
+  geom_point(data=res4, aes(BPcum, -log10(NPQmaxme), colour=as.character(chromosome))) + 
+  scale_color_d3() + 
+  scale_x_continuous(label = axis.set$chromosome, breaks = axis.set$center) + 
+  theme(legend.position = "none") + 
+  ylab("-log10(p-value)") +  
+  xlab("Chromosome")
+
+g1 + g2 + g3 + g4 + plot_annotation(tag_levels = "a")
+
+blup <- fread("../Data/Maize_NPQ_Natural_Variation/data/work/blups/blup2020gwasRNA.csv")
+blup2 <- fread("../Data/Maize_NPQ_Natural_Variation/data/work/blups/blup2021gwasRNA.csv")
+ex <- fread("../Data/Maize_NPQ_Natural_Variation/data/work/expression/WiDiv_Expression_MetaData_csv.gz")
+
+a <- ex[,c("Taxa", "Study", "Zm00001d042697")]
+
+a <- merge(a, blup[,c("Taxa", "NPQmaxme")], by="Taxa")
+colnames(a)[4] <- "NPQmax_2020"
+a <- merge(a, blup2[,c("Taxa", "NPQmaxme")], by="Taxa")
+colnames(a)[5] <- "NPQmax_2021"
+a$year <- str_split_fixed(a$name, "_", 2)[,2]
+
+a <- a %>% pivot_longer(cols = 4:5)
+a <- a[!a$Study=="WiDiv-942**",]
+
+ggplot(a, aes(Zm00001d042697, value)) + 
+  geom_point() + 
+  geom_smooth(method = "lm") + 
+  facet_grid(Study~year) + 
+  xlab(expression(italic(PsbS)~(FPKM))) + 
+  ylab(expression(NPQ[italic(max)]))
+
+
+ggplot() + 
+  geom_hline(yintercept = -log10(0.05/nrow(res2)) , linetype=2) +
+  geom_point(data=res2, aes(BPcum, -log10(res2[,17]), colour=as.character(chromosome))) + 
+  scale_color_d3() + 
+  scale_x_continuous(label = axis.set$chromosome, breaks = axis.set$center) + 
+  theme(legend.position = "none") + 
+  ylab("-log10(p-value)") +  
+  xlab("Chromosome")
