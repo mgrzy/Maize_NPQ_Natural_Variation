4626eab47957dc9e69530269c12cef36686a2e33
Marcin Grzybowski <mgrzy@protonmail.com>
2023-02-17 16:22:02 +0100

    up

---
+++ a.R
@@ -0,0 +1,6 @@
+library(data.table)
+library(tidyverse)
+
+LD_rs3_176152936 <- fread("../Data/Maize_NPQ_Natural_Variation/data/LD/LD_rs3_176152936.ld")
+
+LD_rs2_182332835 <- fread("../Data/Maize_NPQ_Natural_Variation/data/LD/LD_rs2_182382835.ld")
--- processing/LD.sh
+++ processing/LD.sh
@@ -1,6 +1,13 @@
 conda activate geno
 
 #1
-plink --bfile ../../BigData/WiDivGeno/RNAseqagpv4/plink/WiDiv752 --chr 3 --from-bp 175652936 --to-bp 178038945 --out data/work/loci/psbs --make-bed
+plink --bfile ../../BigData/Maize/v4/Genotypes/WiDiv_RNAseq/plink/WiDiv752 --chr 3 --from-bp 175652936 --to-bp 178038945 --out ../Data/Maize_NPQ_Natural_Variation/data/LD/rs3_176152936 --make-bed
 
-plink --bfile data/work/loci/psbs --r2 --ld-snp rs3_176152936 --ld-window-kb 4000 --ld-window 99999 --ld-window-r2 0 --out data/work/loci/LD_rs3_176152936
+plink --bfile ../Data/Maize_NPQ_Natural_Variation/data/LD/rs3_176152936 --r2 --ld-snp rs3_176152936 --ld-window-kb 4000 --ld-window 99999 --ld-window-r2 0 \
+--out ../Data/Maize_NPQ_Natural_Variation/data/LD/LD_rs3_176152936
+
+plink --bfile ../../BigData/Maize/v4/Genotypes/WiDiv_RNAseq/plink/WiDiv752 --chr 2 --from-bp 181882835 --to-bp 182882835 \
+--out ../Data/Maize_NPQ_Natural_Variation/data/LD/rs2_182382835 --make-bed
+
+plink --bfile ../Data/Maize_NPQ_Natural_Variation/data/LD/rs2_182382835 --r2 --ld-snp rs2_182382835 --ld-window-kb 4000 --ld-window 99999 --ld-window-r2 0 \
+--out ../Data/Maize_NPQ_Natural_Variation/data/LD/LD_rs2_182382835
