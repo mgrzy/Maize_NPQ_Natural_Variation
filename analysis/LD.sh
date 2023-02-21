2d933a7b260a8fac22237853ec8f0f5a95b0f899
Marcin Grzybowski <mgrzy@protonmail.com>
2023-02-21 13:37:34 +0100

    up

--- analysis/LD.sh
+++ analysis/LD.sh
@@ -1,6 +1,6 @@
 conda activate geno
 
-#1
+### PSI-PSBS
 plink --bfile ../../BigData/Maize/v4/Genotypes/WiDiv_RNAseq/plink/WiDiv752 --chr 3 --from-bp 175652936 \
 --to-bp 178038945 --out ../Data/Maize_NPQ_Natural_Variation/data/LD/rs3_176152936 --make-bed
 
@@ -8,8 +8,7 @@ plink --bfile ../Data/Maize_NPQ_Natural_Variation/data/LD/rs3_176152936 --r2 --l
 --ld-window-kb 4000 --ld-window 99999 --ld-window-r2 0 \
 --out ../Data/Maize_NPQ_Natural_Variation/data/LD/LD_rs3_176152936
 
-###
-
+###  IRM1 
 plink --bfile ../../BigData/Maize/v4/Genotypes/WiDiv_RNAseq/plink/WiDiv752 --chr 2 --from-bp 182332835 \
 --to-bp 182432835 --out ../Data/Maize_NPQ_Natural_Variation/data/LD/rs2_182382835 --make-bed
 
@@ -17,8 +16,15 @@ plink --bfile ../Data/Maize_NPQ_Natural_Variation/data/LD/rs2_182382835 --r2 --l
 --ld-window-kb 4000 --ld-window 99999 --ld-window-r2 0 \
 --out ../Data/Maize_NPQ_Natural_Variation/data/LD/LD_rs2_182382835
 
-###
+###TRX-Y1
+plink --bfile ../../BigData/Maize/v4/Genotypes/WiDiv_RNAseq/plink/WiDiv752 --chr 3 --from-bp 147481265 \
+--to-bp 147581265 --out ../Data/Maize_NPQ_Natural_Variation/data/LD/rs3_147531265 --make-bed
 
+plink --bfile ../Data/Maize_NPQ_Natural_Variation/data/LD/rs3_147531265 --r2 --ld-snp rs3_147531265 \
+--ld-window-kb 4000 --ld-window 99999 --ld-window-r2 0 \
+--out ../Data/Maize_NPQ_Natural_Variation/data/LD/LD_rs3_147531265
+
+### ACHT3
 plink --bfile ../../BigData/Maize/v4/Genotypes/WiDiv_RNAseq/plink/WiDiv752 --chr 7 --from-bp 179231748 \
 --to-bp 179331748 --out ../Data/Maize_NPQ_Natural_Variation/data/LD/rs7_179281748 --make-bed
 
