efe01c998b90e69635c18a0c49c9265fd7c19b23
Marcin Grzybowski <mgrzy@protonmail.com>
2023-03-08 17:11:04 +0100

    up

--- analysis/LD_V4_v5.R
+++ analysis/LD_V4_v5.R
@@ -21,3 +21,28 @@ PSI3v5 <- data.frame(Taxa=dat2$V1,
                      chr3_178407565=geno2[which(map2$SNP=="chr3_178407565"),])
 
 a <- merge(PSI3v4, PSI3v5, by="Taxa")
+
+### IRM1
+IRM1v4 <- data.frame(Taxa=dat$V1,
+                     rs2_182382835=geno[which(map$SNP=="rs2_182382835"),])
+
+IRM1v5 <- data.frame(Taxa=dat2$V1,
+                     chr2_181981544=geno2[which(map2$SNP=="chr2_181981544"),],
+                     chr2_181981600=geno2[which(map2$SNP=="chr2_181981600"),],
+                     chr2_181981652=geno2[which(map2$SNP=="chr2_181981652"),],
+                     chr2_181982035=geno2[which(map2$SNP=="chr2_181982035"),])
+
+irm1 <- merge(IRM1v4, IRM1v5, by="Taxa")
+
+###ACHT3
+
+ACHT3v4 <- data.frame(Taxa=dat$V1,
+                     rs3_176152936=geno[which(map$SNP=="rs7_179281748"),])
+
+ACHTv5 <- data.frame(Taxa=dat2$V1,
+                     chr7_182742647=geno2[which(map2$SNP=="chr7_182742647"),],
+                     chr7_182742793=geno2[which(map2$SNP=="chr7_182742793"),],
+                     chr7_182742814=geno2[which(map2$SNP=="chr7_182742814"),],
+                     chr7_182742836=geno2[which(map2$SNP=="chr7_182742836"),])
+
+acht <- merge(ACHT3v4, ACHTv5, by="Taxa")
